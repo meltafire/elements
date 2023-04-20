ec29e96350e1691d05e113f3d93d55f794fef266
meltafire <meltafire@gmail.com>
2023-04-20 14:14:33 +0300

    F! MenuProvider binding

---
+++ Assets/Scripts/Installers/Features/MenuInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Elements.Menu.Providers.Implementation;
+using Elements.Menu.Providers.Infrastructure;
+using UnityEngine;
+using Zenject;
+
+namespace Elements.Installers.Features
+{
+    public class MenuInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private MenuProviderView _menuProviderView;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IMenuProvider>().FromInstance(_menuProviderView);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/Providers/Implementation/MenuProviderView.cs
@@ -0,0 +1,35 @@
+﻿using Elements.Menu.Providers.Infrastructure;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Elements.Menu.Providers.Implementation
+{
+    public class MenuProviderView : MonoBehaviour, IMenuProvider
+    {
+        [SerializeField]
+        private Button _button;
+
+        public event Action OnNextButtonClick;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(HandleButtonPressed);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(HandleButtonPressed);
+        }
+
+        public void SetButtonActive(bool isActive)
+        {
+            _button.interactable = isActive;
+        }
+
+        private void HandleButtonPressed()
+        {
+            OnNextButtonClick?.Invoke();
+        }
+    }
+}
