b0f027828baada1f2cf413a1765972f17b2a4936
meltafire <meltafire@gmail.com>
2023-04-20 09:31:22 +0300

    F! GameEndRulesHandler

---
+++ Assets/Scripts/GameSession/Handlers/Implementation/GameEndRulesHandler.cs
@@ -0,0 +1,25 @@
+﻿using Elements.GameSession.Handlers.Infrastructure;
+using UnityEditor;
+
+namespace Elements.GameSession.Handlers.Implementation
+{
+    public class GameEndRulesHandler : IGameEndRulesHandler
+    {
+        private readonly ILevelContainer _levelContainer;
+
+        public bool CheckGameCompletion()
+        {
+            var containers = _levelContainer.PositionContainers;
+
+            foreach (var container in containers)
+            {
+                if(!container.IsEmpty())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
